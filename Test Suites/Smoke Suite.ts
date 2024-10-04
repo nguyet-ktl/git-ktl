0a3b137f3e65f9b25bc1d6a83fff93cc3e953eb5
nguyet.hoang <nguyet.hoang@localhost>
2024-10-04 13:48:24 +0700

    Commiting test case and test suite

---
+++ Test Suites/Smoke Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>83c7c0fe-09a2-4be8-8954-b886a553090b</testSuiteGuid>
+</TestSuiteEntity>
